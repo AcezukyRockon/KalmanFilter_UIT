7ed1531c79c1bc8a97b18491d5afc8575d0abdd4
rock-on <36922874+AcezukyRockon@users.noreply.github.com>
2022-04-20 16:18:55 +0700

    plot 2 datas instead 1

--- plot.py
+++ plot.py
@@ -4,7 +4,10 @@
 import numpy  as np
 import matplotlib.pyplot as plt
 data = np.loadtxt('input.txt')
+data2 = np.loadtxt('output.txt')
 
 x = data[:]
+x2 = data2[:]
 plt.plot(x)
+plt.plot(x2)
 plt.show()
